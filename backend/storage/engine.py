a5ab63be94eb7fc5b59960169b0efd84500f3a94
Nasir Abdulrasheed <mypythtesting@gmail.com>
2023-06-04 23:47:05 +0100

    DB ENGINE: added match_users method to filter users and some pycodestyle changes

--- backend/storage/engine.py
+++ backend/storage/engine.py
@@ -26,36 +26,42 @@ class Engine:
     def load(self):
         connect('popchat')
 
+    def match_users(self, search_term: str = '') -> list[User]:
+        """fetches a list of users that match the search term"""
+        return User.objects(username__icontains=search_term)
+
     def get_by_id(self, model: Type[T], id: str) -> T | None:
         """fetches a document by id"""
         return model.objects(id=id).first()
-    
+
     def get_by_username(self, name: str) -> User | None:
         """fetches a user by username"""
         return User.objects(username=name).first()
-    
+
     def get_by_email(self, email: str) -> User | None:
         """fetches a user by email"""
         return User.objects(email=email).first()
-    
+
     def get_by_auth_token(self, token: str) -> User | None:
         """fetches a user by auth token"""
         return User.objects(auth_token=token).first()
-    
+
     def get_by_reset_token(self, token: str) -> User | None:
         """fetches a user by reset token"""
         return User.objects(reset_token=token).first()
-    
-    def get_chats_by_user(self, user: User) -> list[Chat | Room]:
+
+    def get_chats_by_user(self, user: User) -> list[Chat]:
         """
         fetches all active chats and rooms for a user
 
         """
-        return Chat.objects(Q(user_1=user) | Q(user_2=user)).exclude('messages')
-    
-    def get_rooms_by_user(self, user: User) -> list[Chat | Room]:
+        return Chat.objects(Q(user_1=user.username) |
+                            Q(user_2=user.username)).exclude('messages')
+
+    def get_rooms_by_user(self, user: User) -> list[Room]:
         """
         fetches all active chats and rooms for a user
 
         """
-        return Room.objects(members__in=[user]).exclude('messages', 'members')
+        return (Room.objects(members__in=[user.username])
+                .exclude('messages', 'members'))
