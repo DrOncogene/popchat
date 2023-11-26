ccbaca02d06c4be26e332c5903260f06576f1717
Nasir Abdulrasheed <mypythtesting@gmail.com>
2023-11-26 13:16:19 +0100

    chore: removed unused scripts

--- dev_test.sql
+++
@@ -1,9 +0,0 @@
--- sets up the test database
-
-CREATE USER IF NOT EXISTS 'popchat_dev'@'localhost' IDENTIFIED BY 'popchat_pwd';
-CREATE DATABASE IF NOT EXISTS popchat_dev_db;
-
-
-GRANT ALL ON `popchat_dev_db`.* TO 'popchat_dev'@'localhost';
-
-FLUSH PRIVILEGES
